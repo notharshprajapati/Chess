312af91c450321a70d1bde7ed0e979625647840e
notharshprajapati <prajapati.harsh0243@gmail.com>
2022-08-01 17:32:16 +0530

    Promote done

--- src/Game.js
+++ src/Game.js
@@ -1,15 +1,78 @@
 import * as Chess from "chess.js";
-import { BehaviorSubject, from } from "rxjs";
+import { BehaviorSubject } from "rxjs";
 
 const chess = new Chess();
 
-export const gameSubject = new BehaviorSubject({
-  board: chess.board(),
-});
+export const gameSubject = new BehaviorSubject();
 
-export function move(from, to) {
-  const legalMove = chess.move({ from, to });
+export function initGame() {
+  const savedGame = localStorage.getItem("savedGame");
+  if (savedGame) {
+    chess.load(savedGame);
+  }
+  updateGame();
+}
+
+export function resetGame() {
+  chess.reset();
+  updateGame();
+}
+
+export function handleMove(from, to) {
+  const promotions = chess.moves({ verbose: true }).filter((m) => m.promotion);
+  if (promotions.some((p) => `${p.from}:${p.to}` === `${from}:${to}`)) {
+    const pendingPromotion = { from, to, color: promotions[0].color };
+    updateGame(pendingPromotion);
+  }
+  const { pendingPromotion } = gameSubject.getValue();
+
+  if (!pendingPromotion) {
+    move(from, to);
+  }
+}
+
+export function move(from, to, promotion) {
+  let tempMove = { from, to };
+  if (promotion) {
+    tempMove.promotion = promotion;
+  }
+  const legalMove = chess.move(tempMove);
+
   if (legalMove) {
-    gameSubject.next({ board: chess.board() });
+    updateGame();
+  }
+}
+
+function updateGame(pendingPromotion) {
+  const isGameOver = chess.game_over();
+
+  const newGame = {
+    board: chess.board(),
+    pendingPromotion,
+    isGameOver,
+    turn: chess.turn(),
+    result: isGameOver ? getGameResult() : null,
+  };
+
+  localStorage.setItem("savedGame", chess.fen());
+
+  gameSubject.next(newGame);
+}
+function getGameResult() {
+  if (chess.in_checkmate()) {
+    const winner = chess.turn() === "w" ? "BLACK" : "WHITE";
+    return `CHECKMATE - WINNER - ${winner}`;
+  } else if (chess.in_draw()) {
+    let reason = "50 - MOVES - RULE";
+    if (chess.in_stalemate()) {
+      reason = "STALEMATE";
+    } else if (chess.in_threefold_repetition()) {
+      reason = "REPETITION";
+    } else if (chess.insufficient_material()) {
+      reason = "INSUFFICIENT MATERIAL";
+    }
+    return `DRAW - ${reason}`;
+  } else {
+    return "UNKNOWN REASON";
   }
 }
